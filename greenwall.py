d87ed654f0601f8080d9c37e2aa01657269a9563
Joel <joel.gotsch@gmail.com>
2020-06-20 19:41:20 +0200

    finished raw, untested version of code. gpio_settings.json needs to be updated then test

--- greenwall.py
+++ greenwall.py
@@ -1,47 +1,169 @@
 import gpiozero
 import json
 import logging
+import datetime
+import sys
+from taskmanager import task_manager
+from collections import OrderedDict
+import operator
 
 class device:
-    def __init__(self, name, gpiopin, blynk_vpin, *args, **kwargs):
+    def __init__(self, name, gpiopin, blynk_vpin, min_pause, logger, *args, **kwargs):
         self.name = name
         self.gpiopin = gpiopin
         self.vpin = blynk_vpin
-        self.gpio_obj = gpiozero.LED(gpiopin)
+        try:
+            self.gpio_obj = gpiozero.LED(gpiopin)
+        except gpiozero.exc.GPIOPinInUse:
+            logger.error("A Gpio-Pin is assigned to at least two devices. Check gpio_settings.json")
+            raise gpiozero.exc.GPIOPinInUse
+        self.min_pause = min_pause
+        self.logger = logger
+        try:
+            with open("last_turned_on.json") as file_settings:
+                last_turned_on = dict(json.load(file_settings))
+        except FileNotFoundError:
+            last_turned_on = dict()
+            last_turned_on[name] = str(datetime.datetime.today()+datetime.timedelta(days=-1))
+        if name not in last_turned_on.keys():
+            last_turned_on[name] = str(datetime.datetime.today()+datetime.timedelta(days=-1))
+            with open('last_turned_on.json', 'w') as outfile:
+                json.dump(last_turned_on, outfile)
+        self.last_date_turned_on = datetime.datetime.strptime(last_turned_on[name], "%Y-%m-%d %H:%M:%S.%f")
+        self.logger.info("Initialized device " + name)
+        
+
+        
 
     def turn_on(self):
+        if datetime.datetime.today <= self.last_date_turned_on + datetime.timedelta(seconds=self.min_pause):
+            self.logger.info("Tried to turn on device '" + self.name + "' before min_pause= " + str(self.min_pause) + " seconds since the last turn on event.")
+            return(0)
         self.gpio_obj.on()
+        self.last_date_turned_on = datetime.datetime.today()
+        self.logger.debug(self.name + ": turned on")
+        # write to last_turned_on
+        with open("last_turned_on.json") as file_settings:
+            last_turned_on = dict(json.load(file_settings))
+        last_turned_on[name] = str(datetime.datetime.today())
+        with open('last_turned_on.json', 'w') as outfile:
+            json.dump(last_turned_on, outfile)
+        return(1)
         #send info to blynk (via thread!)
 
     def turn_off(self):
         self.gpio_obj.off()
+        self.logger.debug(self.name + ": turned off")
         #send info to blynk
 
     def get_value(self):
         return(self.gpio_obj.value)
     
-    #define 
-    
+    def get_target_value(self):
+        """Returns the state in which the device should be at the current time, derived from last set value in plans.json for device with this name.
+        Returns None if Dive is not described in plans.json"""
+        with open("plans.json") as file_plans:
+            plans_dict = OrderedDict(json.load(file_plans))
+        # (1)filter for actions for that device, save plan
+        # (2)calculate last time action should have been set for each action and save latest of possible times
+        # (3) step through actions of plan and check if action started/finished and if device was used.
+        #  if action = turn_on, then value = 1. But if duration is given, then flip value (1-value)
+
+        #(1); contains the whole action, (is_active, description, weekdays, start_time,..)
+        filtered_plans = OrderedDict()
+        for plan in plans_dict if plans_dict[plan]["is_active"] is True and \
+            any([self.name in devices for devices in plans_dict[plan]["actions"]])):
+            filtered_plans[plan]= plans_dict[plan]
+        #(2)
+        last_plan=OrderedDict(last_time=datetime.datetime.min)
+        for ac in filtered_plans:
+            filtered_plans[ac]["last_time"] = calc_last_runtime(wdays=filtered_plans[ac]["weekdays"], start_time=filtered_plans[ac]["start_time"])
+            if filtered_plans[ac]["last_time"]>last_plan["last_time"]:
+                last_plan=filtered_plans[ac]
+
+        if len(filtered_plans)==0:
+            self.logger.debug("All actions filtered in function get_target_value for device " + self.name)
+            return(None)
+        #(3)
+        last_time = last_plan["last_time"]
+        last_action_task = ""
+        last_action_finished = False
+        #last_action_started = False
+        for action in last_plan["actions"].keys():
+            if "duration" in last_plan["actions"][action].keys():
+                last_time_new = last_time + datetime.timedelta(seconds=last_plan["actions"][action]["duration"])
+            if self.name in last_plan["actions"][action]["devices"]:
+                #last_action_started=True
+                last_action_task=last_plan["actions"][action]["task"]
+            if last_time_new > datetime.datetime.today():
+                last_action_finished = last_action_finished and (self.name not in last_plan["actions"][action]["devices"])
+                if last_action_task == "":
+                    # this is the case where the plan already started, but the task which involves the device didn't start yet.
+                    # E.g. Now is 11:01 and the device is a small pump, the pumping plan starts with the big pump for 10s at 11:00,
+                    # so the small pump didn't start.
+                    # action which is coming will be used where the value will be flipped due to last_action_finished=False.
+                    # this is an assumption! correct would be to look up the previous action in filtered_plans.
+                    # In the example above it works out.. If this should be improved, probably a last_action_started variable is needed to determine
+                    # one needs to use the previous action
+                    last_action_task = [last_plan[action]["task"] for action in last_plan["actions"]][0]
+                else:
+                    break
+            else:
+                last_time=last_time_new
+                last_action_finished = last_action_finished or (self.name in last_plan["actions"][action]["devices"])
+
+        if last_action_task=="turn_on":
+            value=1
+        elif last_action_task=="turn_off":
+            value=0
+        elif:
+            self.logger.exception("While checking the target value, task '" + last_action_task + "' for device "+ self.name + " was tried, which isn't defined.")
+            return(None)
+        if not last_action_finished:
+            value = 1-value
+        return(value)
+        
+
 
 class greenwall:
     """ Contains all the appliances on the wall such as the pumps, the light and the camera """
-    def __init__(self, gpio_settings, name, platform, *args, **kwargs):
+    def __init__(self, name, platform, logger=None, *args, **kwargs):
         self.name = name
         self.platform = platform # Raspberry or Onion
         self.devices = []
         # init logger
-        # check if all devices are in the right status; according to actions.json
-        # should taskmanager be a part of greenwall? Or vice versa? Or in parallel?
+        if logger is None:
+            logging.basicConfig(filename='logs/green_wall.log', level=logging.DEBUG, format='[%(asctime)s] [%(name)s] [%(levelname)s] %(message)s')
+            logger = logging.getLogger('green_wall')
+            ch = logging.StreamHandler(sys.stdout)
+            ch.setLevel(logging.INFO)
+            logger.addHandler(ch)
+            logger.setLevel(logging.INFO)
+        self.logger=logger
+        # read device list from gpio_settings.json:
+        with open("gpio_settings.json") as file_settings:
+            gpio_settings = dict(json.load(file_settings))
+        for k in gpio_settings.keys():
+            curr_device = gpio_settings[k]
+            self.devices.append(device(name=curr_device["name"], gpiopin=curr_device["gpio_pin"], blynk_vpin=curr_device["vpin"], min_pause=curr_device["min_pause"], logger=self.logger))
+        # check if all devices are in the right status; according to plans.json and send current state to blynk
+        for dev in self.devices:
+            tv = dev.get_target_value
+            if  tv is not None and tv != dev.get_value:
+                self.logger.info("Device '" + dev.name + "' was in the wrong state and value is now set to " + str(tv))
+                if tv == 1
+                    dev.turn_on()
+                else:
+                    dev.turn_off()
 
+        self.logger.info("Initiated green wall named '" + name + "' on " + platform)
 
     def get_device_name(self, name):
         """returns a device object when you ask for the name"""
-        return(self.devices[name])
-        
-        # for dev in self.devices:
-        #     if dev.name == name:
-        #         return(dev)
-        # raise KeyError
+        for dev in self.devices:
+            if dev.name == name:
+                return(dev)
+        raise KeyError
 
     def get_device_vpin(self, vpin):
         """returns a device object when you ask for its vpin"""
@@ -55,5 +177,23 @@ class greenwall:
         dev = self.get_device_vpin(vpin)
         return(dev.get_value())
 
+def prev_date_wday(wday):
+    """ returns datetime object of last date at 00:00 which has the weekday given by wday.
+    E.g. if today is Friday, 20th of June and wday = 0, then it will return Monday, 15th of June 00:00"""
+    today = datetime.datetime.today().date()
+    tw = today.weekday()
+    return(datetime.datetime.combine(today + datetime.timedelta(days= -((tw-wday)%7)), datetime.datetime.min.time()))
 
-    
+def calc_last_runtime(wdays, start_time):
+    hours, minutes = plan["start_time"].split(":")
+    possible_times = [prev_date_wday(wday)+datetime.timedelta(hours=float(hours), minutes=float(minutes)) for wday in wdays]
+    return(max(possible_times))
+
+
+
+
+if __name__ == "__main__":
+    gw = greenwall(name="Joels Green Wall :)", platform="Raspberry Pi 4")
+    tm = task_manager(green_wall=gw)
+    tm.run()
+
--- taskmanager.py
+++ taskmanager.py
@@ -1,11 +1,26 @@
 import logging
 import sys
-
 from threading import Timer
 import datetime
+import json
+import time
+from collections import OrderedDict
+#TODO: Rename such that plan>action>task
+def get_duration(action):
+    if "duration" in action.keys():
+        return(action["duration"])
+    else:
+        return(0)
+
+def next_date_wday(wday):
+    """ returns datetime object of next date at 00:00 which has the weekday given by wday.
+    E.g. if today is Friday, 20th of June and wday = 0, then it will return Monday, 22nd of June 00:00"""
+    today = datetime.datetime.today().date()
+    tw = today.weekday()
+    return(datetime.datetime.combine(today + datetime.timedelta(days=(wday-tw)%7), datetime.datetime.min.time()))
 
 class task:
-    def __init__(self, exec_time, func, name = None, logger = None, *args, **kwargs):
+    def __init__(self, exec_time, func, name=None, logger=None, *args, **kwargs):
         self.exec_time = exec_time
         self.name = name
         self.func = func
@@ -21,6 +36,9 @@ class task:
             logger.setLevel(logging.INFO)
             self.logger = logger
 
+    def __str__(self):
+        return("Execute function " + self.name + " at " + str(self.exec_time))
+
     def run(self):
         delta_t = (self.exec_time-datetime.datetime.today()).total_seconds()
         self.logger.debug("Running Job '"+ self.name + "' in " + str(delta_t) + " seconds, that is at " + str(self.exec_time))
@@ -30,23 +48,62 @@ class task:
 class task_manager:
     """ makes sure that tasks are being executed without blocking the main thread and without permanent checking while still allowing
     for external steering (e.g. via blynk-app)"""
-    def __init__(self, is_active, description, weekdays, start_time, devices, actions, *args, **kwargs):
-        # initialize task_list by parsing actions.json and run it.
+    def __init__(self, green_wall, logger=None, *args, **kwargs):
+        # initialize task_list by parsing plans.json and run it.
         self.task_list = []
-        logger = logging.getLogger('task_manager')
-        ch = logging.StreamHandler(sys.stdout)
-        ch.setLevel(logging.INFO)
-        formatter = logging.Formatter('[%(asctime)s] [%(name)s] [%(levelname)s] %(message)s')
-        ch.setFormatter(formatter)
-        logger.addHandler(ch)
-        logger.setLevel(logging.INFO)
+        if logger is None:
+            logging.basicConfig(filename='logs/task_manager.log', level=logging.DEBUG, format='[%(asctime)s] [%(name)s] [%(levelname)s] %(message)s')
+            logger = logging.getLogger('task_manager')
+            ch = logging.StreamHandler(sys.stdout)
+            ch.setLevel(logging.INFO)
+            
+            formatter = logging.Formatter('')
+            ch.setFormatter(formatter)
+            logger.addHandler(ch)
+            logger.setLevel(logging.INFO)
         self.logger = logger
+        self.green_wall = green_wall
+        self.create_task_list()
 
     def create_task_list(self):
         # updates the task-list for the next 12h including an update at the end of the cycle for itself.
         # the task-list contains all actions like starting and stopping pumps. It needs to be sorted, so that the first task 
         # in the list is the next one to do. The last task is creating the task_list again (for the next 12h).
-        pass
+        plan_time_start = datetime.datetime.today()
+        plan_time_end = plan_time_start + datetime.timedelta(hours=12.0)
+        with open("plans.json") as file_plans:
+            plans_dict = OrderedDict(json.load(file_plans))
+
+        for plan in plans_dict:
+            if not plan['is_active']:
+                continue
+            # check if one of weekday + time combinations is between plan_time_start and plan_time_end
+            hours, minutes = plan["start_time"].split(":")
+            possible_times = [next_date_wday(wday)+datetime.timedelta(hours=float(hours), minutes=float(minutes)) for wday in plan['weekdays']]
+            duration_adjustment = sum([get_duration(action) for action in plan["actions"]]) # necessary if action is running currently
+            #if it should, it may re-run the commands which ran before already, e.g. the big pump may run a 2nd time
+            plan_times = [possible_time for possible_time in possible_times if plan_time_start + datetime.timedelta(seconds=-duration_adjustment) < possible_time < plan_time_end]
+            if len(plan_times) <= 0:
+                continue
+            for plan_time in plan_times:
+                for action_name in plan["actions"].keys():
+                    action = plan["actions"][action_name]
+                    for device in action["devices"]:
+                        dev = self.green_wall.get_device_name(device)
+                        if action["task"] == "turn_on":
+                            task1 = dev.turn_on
+                            task2 = dev.turn_off
+                        elif action["task"] == "turn_off":
+                            task1 = dev.turn_off
+                            task2 = dev.turn_on
+                        elif:
+                            self.logger.exception("The specified action '"+ str(action["task"])+"' is not implemented.")
+                        self.add_task(task(exec_time=plan_time, func=task1, name=action_name, logger=self.logger))
+                        if "duration" in action.keys():
+                            self.add_task(task(exec_time=plan_time+datetime.timedelta(seconds=action["duration"]), func=task2, name=action_name, logger=self.logger))
+                    if "duration" in action.keys():
+                        plan_time = plan_time + datetime.timedelta(seconds=action["duration"])
+        self.add_task(task(exec_time=plan_time_end, func=self.create_task_list, name="Update Task List", logger=self.logger))
 
     def run(self):
         #search for task wich should be executed next, wrap it's function and execute
@@ -54,9 +111,16 @@ class task_manager:
         next_task.func = self.wrap_task(next_task.func)
         next_task.run()
 
-    def add_task(self):
+    def add_task(self, task):
         # add task to task_list with appropriate position (according to exec_time)
-        pass
+        self.logger.debug("Added task: " + str(task))
+        i=0
+        if len(self.task_list)==0:
+            self.task_list.insert(i,task)
+            return()
+        while task.exec_time > self.task_list[i].exec_time:
+            i+=1
+        self.task_list.insert(i,task)
 
     def wrap_task(self, func):
 
@@ -68,4 +132,9 @@ class task_manager:
             self.run()
 
         return(return_func(func))
+
+
+if __name__ == "__main__":
+    print("hello world")
+
 
